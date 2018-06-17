7bfb04adb1a0d6b1a839683072387156762a5a1a
marseli95 <marsel.piroli@gmail.com>
2018-06-17 14:18:50 +0200

    css :wq

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
-echo "kjo eshte faqja e 20 php";
-?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+<h1>BRENDA NJE KOMPJUTERI </h1>
+
+</body>
+</html>
+
