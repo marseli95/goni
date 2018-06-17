b9dad26385d70e25bd0fd49b2e277dadf84adf44
marseli95 <marsel.piroli@gmail.com>
2018-06-17 21:17:32 +0200

    tlovers

--- index.php
+++ index.php
@@ -4,18 +4,77 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Pjeset e kompjuterit</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <script src="main.js"></script>
 </head>
 <body>
+<div class="grid">
 <div class="site-header">
-<h1>BRENDA NJE KOMPJUTERI </h1>
+<h1>BRENDA NJE KOMPJUTERI .</h1>
 </div>
-<div class="content" col-md-6 offset-2>
+<div class="site1">
+    <h4>
 A keni parë ndonjëherë brenda një  kompjuteri, ose keni parë figura të brendësisë së tij? Një pamje kompjuteri nuk është aspak misterioz. Ky mësim do t'ju ndihmojë të zotni disa terminologjinë bazë dhe të kuptoni se si të punoni me nje kompjuter.
+</h4>
+</div>
+<div class="site2">
+<iframe width="1000" height="415" src="https://www.youtube.com/embed/4eNTlwnnhss" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+</div>
+<div class="site3">
+    <h1>Matherboard</h1>
+    <h4>
+Motherboard është bordi kryesor i kompjuterit. Është një pllakë që mban CPU, memorie, lidhje për hard drive dhe disqet optike, karta zgjerimi për të kontrolluar video dhe audio dhe lidhje me portat e kompjuterit tuaj (siç janë portat USB). Motherboard lidh direkt ose indirekt në çdo pjesë të kompjuterit.
+</h4>
+<img src="matherboard.jpg" alt="">
+</div>
+<div class="site4">
+    <h1>CPU/processor</h1>
+    <img src="cpu.jpg" alt="">
+    <h4>Njësia qendrore e përpunimit (CPU), e quajtur edhe procesori, gjendet brenda në rastin e kompjuterit në motherboard. Nganjëherë quhet truri i kompjuterit. Sa herë që shtypni një çelës, klikoni miun, ose nisni një aplikacion, po dërgoni udhëzime në CPU.
 
+CPU është zakonisht një shesh qeramike me dy inç me një çip silikon të vendosur brenda. Çipi është zakonisht për madhësinë e një tabloje. CPU përshtatet priza e CPU, e cila është e mbuluar nga lavamani, një objekt që thith ngrohjen nga CPU.
+
+Shpejtësia e një procesori matet në megahertz (MHz) ose miliona instruksione për sekondë; dhe gigaherz (GHz), ose miliarda instruksione për sekondë. Një procesor më i shpejtë mund të ekzekutojë udhëzimet më shpejt. Megjithatë, shpejtësia aktuale e kompjuterit varet nga shpejtësia e shumë komponentëve të ndryshëm.</h4>
+</div>
+<div class="site5">
+    <h1>RAM (random access memory)</h1>
+    <img src="ram.jpg" alt="">
+    <h4>RAM është memorie afatshkurtër e sistemit tuaj. Sa herë që kompjuteri juaj kryen llogaritje, ai përkohësisht i ruan të dhënat në RAM derisa të jetë e nevojshme.
+
+Ky memorie afatshkurtër zhduket kur kompjuteri është i fikur. Nëse jeni duke punuar në një dokument, spreadsheet ose një lloj tjetër skedari, do t'ju duhet ta ruani. Kur ruani një skedar, të dhënat shkruhen në hard drive, e cila vepron si ruajtje afatgjatë.
+
+RAM matet në megabajt (MB) ose gigabajt (GB). Sa më shumë që keni, aq më shumë mund të bëni në të njëjtën kohë. Nëse nuk keni RAM të mjaftueshme, mund të vëreni se kompjuteri juaj është i ngadaltë kur keni disa programe të hapura. Për shkak të kësaj, shumë njerëz shtojnë RAM shtesë në kompjuterët e tyre për të përmirësuar performancën.</h4>
+</div>
+<div class="site6">
+    <h1>Hard drive</h1>
+    <img src="hd.jpg" alt="">
+    <h4>Hard drive është vendi ku ruhen programet, dokumentet dhe skedarët e tjerë. Hard drive është ruajtje afatgjatë, që do të thotë që të dhënat ruhen akoma edhe nëse e ndizni kompjuterin ose hiqeni nga priza.
+
+Hard drive mbi RAM kur ju drejtuar një program ose hapur një skedar. Kur ruani një skedar, të dhënat kopjohen përsëri në hard drive. Sa më shpejt të jetë hard drive, aq më shpejt kompjuteri mund të fillojë dhe të ngarkojë programe.</h4>
+</div>
+<div class="site7">
+    <h1>Njësia e furnizimit me energji elektrike</h1>
+    <img src="enrg.jpg" alt="">
+    <h4>Njësia e furnizimit me energji në kompjuter konverton furnizimin me energji nga kompjuteri. Ai dërgon energji përmes kabllove në motherboard dhe komponentë të tjerë.</h4>
+
+</div>
+<div class="site8">
+    <h1>Video card</h1>
+    <img src="video.jpg" alt="">
+    <h4>Karta grafike është përgjegjëse për atë që shihni në monitor. Shumica e kompjuterëve kanë një GPU (njësi grafike të përpunimit) të ndërtuar në motherboard, në vend që të ketë një kartë të veçantë video. Nëse ju pëlqen të luani lojëra grafike-intensive, ju mund të shtoni një video më të mirë.</h4>
+</div>
+<div class="site9">
+    <h1>Sound card</h1>
+    <img src="sound.jpg" width="300px" height="300px" alt="">
+    <h4>Karta e zërit - e quajtur edhe një kartë audio - është përgjegjës për atë që dëgjoni në altoparlantë ose kufje. Shumica e motherboards kanë tinguj të integruar, por ju mund të përmirësoni në një kartë zanore për tinguj me cilësi të lartë.</h4>
+</div>
+<div class="site10">
+    <h1>Network card</h1>
+    <img src="net.jpg" alt="">
+    <h4>Karta e rrjetit lejon kompjuterin tuaj të komunikojë nëpërmjet një rrjeti dhe të ketë qasje në internet. Mund të lidhet me një kabllo Ethernet ose nëpërmjet një lidhjeje wireless (shpesh të quajtur Wi-Fi). Shumë nga motherboards kanë lidhje të rrjetit të integruar, dhe një kartë rrjeti mund të shtohet në një vend të zgjerimit.</h4>
+</div>
 </div>
 </body>
 </html>
